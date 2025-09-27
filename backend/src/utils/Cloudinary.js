3e470dcc310cead6991408b357d50c8d882493c1
shubham maurya <shubhammaurya9208@gmail.com>
2025-09-28 01:07:31 +0530

    cloudinary & multer for file upload

---
+++ backend/src/utils/Cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name: process.env.CLODINARY_CLOUD_NAME,
+    api_key: process.env.CLODINARY_API_KEY,
+    api_secret: process.env.CLODINARY_KEY_SECRET
+});
+
+const uploadImagetoCloudinary = async (filePath) => {
+    try {
+        if(!filePath) return null
+        const response= await cloudinary.uploader.upload(filePath,{
+            resource_type: 'auto',
+        })
+        console.log('after file uploded successfully',response.url);
+        return response
+        
+    } catch (error) {
+        fs.unlinkSync(filePath) // remove file from local uploads folder for our server cleanup and fs is filesystem module it comes with nodejs by default unlinkSync is used to delete file synchronously means hona hi chiye ye
+        console.log('error while uploading file to cloudinary',error);
+        return null
+    }
+}
+
+export { uploadImagetoCloudinary }
